f968518b19542f1f0d4c7ce588fcd8bc5df464bc
Yunsik Park <space214@gmail.com>
2017-07-05 13:37:44 +0900

    DATABASES세팅 debug,deploy분리 및 설정변경, static_root관련 수정

--- django_app/config/settings/debug.py
+++ django_app/config/settings/debug.py
@@ -7,11 +7,22 @@ WSGI_APPLICATION = 'config.wsgi.debug.application'
 
 # Static URLs
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 
 # 디버그 모드니깐 DEBUG는 True
 DEBUG = True
 ALLOWED_HOSTS = config_secret_debug['django']['allowed_hosts']
+
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 print('@@@ DEBUG:', DEBUG)
--- django_app/config/settings/deploy.py
+++ django_app/config/settings/deploy.py
@@ -7,12 +7,15 @@ WSGI_APPLICATION = 'config.wsgi.deploy.application'
 
 # Static URLs
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 
 # 배포모드이기 때문에 DEBUG는 False
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
 
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+DATABASES = config_secret_deploy['django']['databases']
 
 print('@@@ DEBUG:', DEBUG)
 print('@@@ ALLOWED_HOSTS:', ALLOWED_HOSTS)
